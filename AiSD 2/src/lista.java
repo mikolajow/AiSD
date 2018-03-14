591d59cb1482a4579633bfdb185f7163a0b21d57
PAPAJ <mikolajkasperek@o2.pl>
2018-03-14 21:30:36 +0100

    drugie laby

---
+++ AiSD 2/src/lista.java
@@ -0,0 +1,10 @@
+
+public class lista {
+	
+	
+	
+	
+	
+}//koniec klasy
+
+
---
+++ AiSD 2/src/test.java
@@ -0,0 +1,55 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		
+		int[] tablica = new int[15];
+		tablica[0] = 25;
+		tablica[1] = 769;
+		tablica[2] = 345;
+		tablica[3] = 769;
+		tablica[4] = 325;
+		tablica[5] = 789;
+		tablica[6] = 35;
+		tablica[7] = 768;
+		tablica[8] = 345;
+		tablica[9] = 678;
+		tablica[10] = 345;
+		tablica[11] = 7698;
+		tablica[12] = 3245;
+		tablica[13] = 4567;
+		tablica[14] = 123;
+		
+		quickSort();
+		
+		for ( Integer i : tablica ) {
+			System.out.println(i);
+		}
+		
+		
+		
+	}//koniec main
+
+	private static void quickSort() {
+		
+		
+		
+		
+		
+	}//koniec quick sorta
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
