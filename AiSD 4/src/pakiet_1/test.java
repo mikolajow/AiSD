32811a020d85b722d0aa653942e3dec9a11c8ce1
PAPAJ <mikolajkasperek@o2.pl>
2018-05-10 10:01:57 +0200

    koniec koniec

--- AiSD 4/src/pakiet_1/test.java
+++ AiSD 4/src/pakiet_1/test.java
@@ -1,5 +1,6 @@
 package pakiet_1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test {
@@ -8,25 +9,43 @@ public class test {
 		// TODO Auto-generated method stub
 		
 		Apostezjon test = new Apostezjon();
+		ArrayList<String> miasta = new ArrayList<>();
+		
+		miasta.add( 	"Angmar"	);
+		miasta.add( 	"Gondolin"	);
+		miasta.add( 	"Isengard"	);
+		miasta.add( 	"Rivendell"	);
+		
+		miasta.add( 	"Bree"		);
+		miasta.add( 	"Trondheim"		);
+		miasta.add( 	"Lusitania"		);
+		miasta.add( 	"Roke"		);
+		
+		miasta.add( 	"Atuan"		);
+		miasta.add( 	"Selidor"		);
+		miasta.add( 	"Havnor"		);
+		miasta.add( 	"Gont"		);
 		
 		
-		/*
 		try ( Scanner s = new Scanner(System.in) ){
 			System.out.println("Podaj Stacjê Pocz¹tkow¹");
 		 	String poczatek = s.nextLine();
 		 	System.out.println("Podaj Stacjê Koñcow¹");
 			String koniec = s.nextLine();
-			test.szukajPolaczenia(poczatek, koniec);
+			
+			if ( miasta.contains(poczatek) && miasta.contains(koniec) ) {
+				test.szukajPolaczenia(poczatek, koniec);
+			}else throw new Exception();
 		}catch ( Exception e) {
 			System.out.println("podano z³e dane");
 		}//koniec try-catch
-		*/
+		
 		
 		
 		
 		//test.szukajPolaczenia( "Lusitania", "Bree");
 		//test.szukajPolaczenia( "Bree", "Selidor");
-		test.szukajPolaczenia( "Atuan", "Gondolin");
+		//test.szukajPolaczenia( "Isengard", "banan");
 		
 		/*
 		System.out.println(test.getMapa().get("Atuan").get("Selidor"));
